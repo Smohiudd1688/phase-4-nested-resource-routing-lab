896e3fd79e35433889f37df5b1d31481031e2d9c
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-08-26 16:05:37 -0700

    commit tree 8b0759a5b92f8c2d3ec71dadd51ea8c44d14b81e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end  
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = Item.create(item_params)
+    user.items << item
+    render json: item, status: 201
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: {error: "Item not found"}, status: 404
   end
 
 end
